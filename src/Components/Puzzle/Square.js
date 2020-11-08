5066d1ecca830b5859bba59ee8f317a21a2ba1ad
jayvanput <jayvanput@gmail.com>
2020-11-08 02:03:38 -0500

    Added backspace handling & tab brings you to next open square/last square in a word.

--- src/Components/Puzzle/Square.js
+++ src/Components/Puzzle/Square.js
@@ -7,6 +7,21 @@ class Square extends React.Component {
     this.handleKeyPress = this.handleKeyPress.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
     this.handleSelect = this.handleSelect.bind(this);
+    this.checkOpenSquare = this.checkOpenSquare.bind(this);
+  }
+
+
+  checkOpenSquare(squareID, offset) {
+    let targetSquare = document.getElementById(squareID)
+    let nextSquare = document.getElementById(squareID + offset)
+    let nextEnd = squareID + offset
+    if (targetSquare.value != "" && (nextSquare.dataset.black == "1" || nextSquare.id % this.props.size == 0)) {
+      document.getElementById(squareID).select()
+    } else if (targetSquare.value != "") {
+      this.checkOpenSquare(nextEnd, offset)
+    } else {
+      targetSquare.select()
+    }
   }
 
   handleKeyPress(e) {
@@ -48,35 +63,66 @@ class Square extends React.Component {
     if (this.props.across_dir_flag) {
       if (e.shiftKey && e.keyCode == 9) {
         e.preventDefault()
-        document.getElementById(this.props.square.prev_tab).select()
+        this.checkOpenSquare(this.props.square.prev_tab, 1)
       } else if (e.keyCode == 9) {
         e.preventDefault()
-        document.getElementById(this.props.square.next_tab).select()
+        this.checkOpenSquare(this.props.square.next_tab, 1)
+        // this.props.square.next_tab
       }
     }
     else {
       if (e.shiftKey && e.keyCode == 9) {
         e.preventDefault()
-        document.getElementById(this.props.square.prev_tab_down).focus()
+        this.checkOpenSquare(this.props.square.prev_tab_down, 15)
       } else if (e.keyCode == 9) {
         e.preventDefault()
-        document.getElementById(this.props.square.next_tab_down).select()
+        this.checkOpenSquare(this.props.square.next_tab_down, 15)
       }
     }
 
     // Handle Square movements
     if (e.keyCode == 37) {
       if ((this.props.square.ID - 1) % this.props.size != 0 && this.props.square.ID != 1) {
-        document.getElementById(this.props.id - 1).select()
+        if (this.props.across_dir_flag) {
+          document.getElementById(this.props.id - 1).select()
+        } else {
+          this.props.onDirChange(1)
+        }
       }
     } else if (((this.props.id - this.props.size) >= 0) && e.keyCode == 38) {
-      document.getElementById(this.props.id - this.props.size).select()
-    } else if (e.keyCode == 39) {
-      if (this.props.square.ID % this.props.size != 0) {
+      if (!this.props.across_dir_flag) {
+        document.getElementById(this.props.id - this.props.size).select()
+      } else {
+        this.props.onDirChange(0)
+      }
+    } else if (e.keyCode == 39 && this.props.square.ID % this.props.size != 0) {
+      if (this.props.across_dir_flag) {
         document.getElementById(this.props.id + 1).select()
+      } else {
+        this.props.onDirChange(1)
       }
     } else if (((this.props.id + this.props.size) < Math.pow(this.props.size, 2)) && e.keyCode == 40) {
-      document.getElementById(this.props.id + this.props.size).select()
+      if (!this.props.across_dir_flag) {
+        document.getElementById(this.props.id + this.props.size).select()
+      } else {
+        this.props.onDirChange(0)
+      }
+    }
+
+    // Handle backspace
+    if (e.keyCode == 8) {
+      document.getElementById(this.props.id).value = ""
+      if (!this.props.across_dir_flag) {
+        if (this.props.id - this.props.size > 0) {
+          document.getElementById(this.props.id - this.props.size).select()
+        } else {
+          document.getElementById(this.props.id).select()
+        }
+      } else {
+        if (this.props.id - 1 >= 0) {
+          document.getElementById(this.props.id - 1).select()
+        }
+      }
     }
   }
 
@@ -101,6 +147,7 @@ class Square extends React.Component {
           onKeyPress={this.handleKeyPress}
           data-black={this.props.square.black}
           maxLength={cell_color == "cell-black" ? 0 : 1}
+          disabled={cell_color == "cell-black" ? true : false}
         ></input>
       </td >
     )
